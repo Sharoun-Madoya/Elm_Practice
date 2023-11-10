963834b5d90212cf8d274c2b64c246fcf58e7e2f
[sharoun <[madoyasharoun@gmail.com]>
2023-11-10 08:42:18 +0300

    :memo: elm doesn't have console.log

--- src/Sub.elm
+++ src/Sub.elm
@@ -2,16 +2,14 @@ add num1 num2 = num1 + num2
 
 five= add 2 3
 
-console.log(five)
+
 
 multiple num3 num4 = num3 * num4
 
 nine = multiple 3 * 3
 
-console.log (nine)
+
 
 fourteen = add five nine
-console.log(fourteen)
 
 four= add 2 (1*2)
-console.log (four)
