1b09e1ba8e19971858adc4fa5c6c73ae363a536d
[sharoun <[madoyasharoun@gmail.com]>
2023-11-10 08:59:35 +0300

    :memo: defining elm types

--- src/Sub.elm
+++ src/Sub.elm
@@ -1,15 +1,21 @@
-add num1 num2 = num1 + num2
 
-five= add 2 3
+module Sub exposing(..)
+add : Int -> Int -> Int
+add num1 num2 =num1 + num2
+
+five:Int
+five= 
+    add 2 3
 
 
-
+multiple : Int -> Int ->Int 
 multiple num3 num4 = num3 * num4
 
-nine = multiple 3 * 3
+nine : Int
+nine = multiple 3  3
 
-
-
+fourteen : Int
 fourteen = add five nine
 
+four :Int
 four= add 2 (1*2)
